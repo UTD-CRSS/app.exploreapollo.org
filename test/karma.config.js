3e320872fa60d58da3fc881345064017ea4a3932
Austin Pray <austin@austinpray.com>
2015-09-10 18:41:57 -0500

    Runs Karma Tests on Sauce Labs

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,28 +1,84 @@
 /*eslint-env node*/
 var webpackConfig = require("../webpack.config.js");
+var _ = require("lodash");
 
-module.exports = function (config) {
-  config.set({
-
-    browsers: [ process.env.CONTINUOUS_INTEGRATION ? "Firefox" : "Chrome" ],
-
-    frameworks: [ "mocha" ],
-
-    files: [
-      "tests.webpack.js"
-    ],
-
+function base() {
+  return {
+    frameworks: ["mocha"],
+    files: ["tests.webpack.js"],
     preprocessors: {
       "tests.webpack.js": [ "webpack", "sourcemap" ]
     },
-
-    reporters: [ "mocha" ],
-
     webpack: webpackConfig,
-
     webpackServer: {
       noInfo: true
     }
+  };
+}
 
+function local() {
+  return _.extend(base(), {
+    browsers: ["Chrome"],
+    reporters: ["mocha"]
   });
+}
+
+function sauceLabs() {
+  var customLaunchers = {
+    sl_chrome: {
+      base: "SauceLabs",
+      browserName: "chrome"
+    },
+    sl_firefox: {
+      base: "SauceLabs",
+      browserName: "firefox"
+    },
+    sl_safari: {
+      base: "SauceLabs",
+      browserName: "safari",
+      platform: "OS X 10.10"
+    },
+    sl_iphone_ios_8: {
+      base: "SauceLabs",
+      browserName: "iphone",
+      version: "8.4",
+      deviceName: "iPhone 6 Plus",
+      deviceOrientation: "portrait"
+    },
+    sl_ie_10: {
+      base: "SauceLabs",
+      browserName: "internet explorer",
+      platform: "Windows 7",
+      version: "10"
+    },
+    sl_ie_11: {
+      base: "SauceLabs",
+      browserName: "internet explorer",
+      platform: "Windows 8.1",
+      version: "11"
+    }
+  };
+
+  return _.extend(base(), {
+    sauceLabs: {
+      testName: "UTD CRSS Apollo SPA Tests"
+    },
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 1,
+    browserNoActivityTimeout: 4 * 60 * 1000,
+    captureTimeout: 4 * 60 * 1000,
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    reporters: ["dots", "saucelabs"],
+    singleRun: true
+  });
+}
+
+module.exports = function (config) {
+  if (process.env.CONTINUOUS_INTEGRATION) {
+    // We are on travis
+    config.set(sauceLabs());
+  } else {
+    config.set(local());
+  }
 };
