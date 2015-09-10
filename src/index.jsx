fd30a82eb93ef9d459d758a0c3314d34b7b6b9d9
Austin Pray <austin@austinpray.com>
2015-09-10 15:19:49 -0500

    Sets up sample tests

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import Router, {RouteHandler, Route, DefaultRoute} from "react-router";
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div>
@@ -12,7 +12,7 @@ class App extends Component {
   }
 }
 
-class Dashboard extends Component {
+export class Dashboard extends Component {
   render() {
     return (
       <div>
