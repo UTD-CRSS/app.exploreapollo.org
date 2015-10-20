dff7373bda689dbbba64f40e9ccf657f334258a5
Austin Pray <austin@austinpray.com>
2015-10-19 22:56:42 -0500

    Travis continuous deploys

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+NODE_ENV=production
+NPM_BIN=$(npm bin)
+
+echo "cleaning up"
+rm -rf dist/*
+
+echo "compiling"
+$NPM_BIN/webpack -p
+
+if ! hash s3_website 2>/dev/null; then
+  echo "installing s3_website"
+  gem install s3_website
+fi
+
+echo "initiating push"
+s3_website push
