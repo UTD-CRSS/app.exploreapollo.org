d2d0c0e7c3be4abcde8d892079242cd25c627fae
Jamie Crisman <Jamie@longest.voyage>
2015-11-10 00:04:58 -0600

    :cake: storyviewer update

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export MomentList from "./MomentList";
 export StoryList from "./StoryList";
+export StoryTimeline from "./StoryTimeline";
 export MomentPlayer from "./MomentPlayer";
 export MomentNote from "./MomentNote";
 export Timeline from "./Timeline";
