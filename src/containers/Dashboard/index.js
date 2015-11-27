4551c0cd17c87b0f4ed43ea00f3cd1c2b6058a9b
Jamie Crisman <Jamie@longest.voyage>
2015-11-27 15:05:58 -0600

    added something to the landing page so its not just hello

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -3,8 +3,29 @@ import React, {Component} from "react";
 export default class Dashboard extends Component {
   render() {
     return (
-      <div ref="helloDiv">
-        Hello!
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <h2 className="text-center">
+              Explore Apollo is the first curated resource for Apollo mission audio and research.
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <h3>
+              Missions
+            </h3>
+            <ul>
+              <li>
+                Apollo 11 <a href="/stories?mission=apollo11">Listen Now</a>
+              </li>
+              <li>
+                More to come in the future
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
