f6f9e4b9b2fa90306f7be8dcf462a31555a4c38e
Austin Pray <austin@austinpray.com>
2016-02-02 14:18:44 -0600

    Show notice when no moments

--- src/components/StoryTimeline/index.js
+++ src/components/StoryTimeline/index.js
@@ -3,80 +3,76 @@ import _ from "lodash";
 import {Link} from "react-router";
 import {HumanReadableMs} from "../";
 
-export class StoryCard extends Component {
-
-  render() {
-    const url = "/moments/moment/" + this.props.firstMomentId + "?story=" + this.props.id;
-    return (
-      <div className="jumbotron text-center" style={{
-        margin: 0
-      }}>
-        <div className="story-timeline-item-content">
-          <h2 className="story-timeline-title">
-            {this.props.title}
-          </h2>
-          <p className="lead">
-            {this.props.description}
-          </p>
-          <div>
-            <div className="story-timeline-play">
-              <Link className="btn btn-lg btn-primary" to={url}>Play All <i className="glyphicon glyphicon-play"></i></Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function PlayAllButton({storyId, momentId}) {
+  const url = "/moments/moment/" + momentId + "?story=" + storyId;
+  return (<div>
+    <div className="story-timeline-play">
+      <Link className="btn btn-lg btn-primary" to={url}>Play All <i className="glyphicon glyphicon-play"></i></Link>
+    </div>
+  </div>);
 }
 
-export class MomentCard extends Component {
-  render() {
-    const url = "/moments/moment/" + this.props.id;
-    return(
-      <div className="story-timeline-item story-item">
-        <div className="story-timeline-item-node"></div>
-        <div className="story-timeline-item-content">
-          <div>
-            <div className="story-timeline-title">
-              {this.props.title}
-            </div>
-            <div className="story-timeline-time">
-              <HumanReadableMs ms={this.props.metStart} />
-            </div>
-          </div>
-          <div>
-            <p className="story-timeline-content">
-              {this.props.content}
-            </p>
-            <div className="story-timeline-play">
-              <a href={url}>Listen <i className="glyphicon glyphicon-play"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
+function NoMomentsNotice() {
+  return (<div className="alert alert-info">
+    This story does not have any moments yet
+  </div>);
 }
 
-export class LandmarkCard extends Component {
-  render() {
-    return(
-      <div className="story-timeline-item landmark-item">
-        <div className="story-timeline-item-node"></div>
-        <div className="story-timeline-item-content">
-          <div>
-            <div className="story-timeline-title">
-              {this.props.title}
-            </div>
-            <div className="story-timeline-time">
-              <HumanReadableMs ms={this.props.metStart} />
-            </div>
-          </div>
+export function StoryCard({storyId, firstMomentId, title, description}) {
+  return (<div className="jumbotron text-center" style={{margin: 0}}>
+    <div className="story-timeline-item-content">
+      <h2 className="story-timeline-title">
+        {title}
+      </h2>
+      <p className="lead">
+        {description}
+      </p>
+      {firstMomentId
+        ? <PlayAllButton momentId={firstMomentId} storyId={storyId} />
+        : <NoMomentsNotice />}
+    </div>
+  </div>);
+}
+
+export function MomentCard({id, title, metStart, content}) {
+  const url = "/moments/moment/" + id;
+  return (<div className="story-timeline-item story-item">
+    <div className="story-timeline-item-node" />
+    <div className="story-timeline-item-content">
+      <div>
+        <div className="story-timeline-title">
+          {title}
+        </div>
+        <div className="story-timeline-time">
+          <HumanReadableMs ms={metStart} />
         </div>
       </div>
-    );
-  }
+      <div>
+        <p className="story-timeline-content">
+          {content}
+        </p>
+        <div className="story-timeline-play">
+          <a href={url}>Listen <i className="glyphicon glyphicon-play"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>);
+}
+
+export function LandmarkCard({id, title, metStart}) {
+  return (<div key={title+id}className="story-timeline-item landmark-item">
+    <div className="story-timeline-item-node"></div>
+    <div className="story-timeline-item-content">
+      <div>
+        <div className="story-timeline-title">
+          {title}
+        </div>
+        <div className="story-timeline-time">
+          <HumanReadableMs ms={metStart} />
+        </div>
+      </div>
+    </div>
+  </div>);
 }
 
 export default class StoryTimeline extends Component {
@@ -108,9 +104,18 @@ export default class StoryTimeline extends Component {
     });
   }
   render() {
+    const {story} = this.props;
+    const {momentList} = story;
+
+    const firstMomentId = (
+      momentList.length > 0
+        ? momentList[0].id
+        : false
+    );
+
     return (
       <div>
-        <StoryCard {...this.props.story} firstMomentId={this.props.story.momentList[0].id}/>
+        <StoryCard {...story} storyId={story.id} firstMomentId={firstMomentId} />
         <div className="col-xs-10 col-xs-offset-2">
           <div className="row story-timeline-container">
             <div className="story-timeline-line"></div>
