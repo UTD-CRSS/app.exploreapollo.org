077a8b84fc1e0ab22a72f99efe4962b1bf3d598b
Austin Pray <austin@austinpray.com>
2016-02-07 16:23:42 -0600

    Refactor Timeline into stateless components

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -2,69 +2,69 @@ import React, {Component} from "react";
 import classNames from "classnames";
 import {HumanReadableMs} from "../";
 
-export class TimelineMessage extends Component {
-  render() {
-    const {name, text, active, startTime} = this.props;
-    return (
-      <a className="list-group-item transcript-item cursor-pointer"
-         onClick={this.props.clickEvent.bind(this, startTime)}>
-        <div>
-          <strong>
-            {name}:
-          </strong>
-          <div className="start-time">
-            {HumanReadableMs({ms: startTime})}
-          </div>
+export function TimelineMessage({name, text, active, startTime, clickEvent}) {
+  return (
+    <a className="list-group-item transcript-item cursor-pointer"
+       onClick={clickEvent.bind(this, startTime)}>
+      <div>
+        <strong>
+          {name}:
+        </strong>
+        <div className="start-time">
+          {HumanReadableMs({ms: startTime})}
         </div>
-        <div>
-          <span className={classNames({"active-transcript": active})}>
-            {active} {text}
-          </span>
-        </div>
-      </a>
-    );
-  }
+      </div>
+      <div>
+        <span className={classNames({"active-transcript": active})}>
+          {active} {text}
+        </span>
+      </div>
+    </a>
+  );
 }
 
-export default class Timeline extends Component {
-
-  renderList() {
-    const {timeline, clickEvent} = this.props;
-    if (!timeline || timeline.length < 1) {
-      return (
-        <div ref="errorMessage" className="alert alert-info">No Messages</div>
-      );
-    }
-    let items = timeline.map((item) => {
-      return (
-        <TimelineMessage
-          key={item.id}
-          id={item.id}
-          name={item.speakerName}
-          active={item.active}
-          clickEvent={clickEvent}
-          startTime={item.metStart}
-          text={item.text} />
-      );
-    });
+function TimelineList({timeline, clickEvent}) {
+  if (!timeline || timeline.length < 1) {
     return (
-      <div className="list-group">
-        {items}
-      </div>
+      <div testRef="errorMessage" className="alert alert-info">No Messages</div>
     );
   }
-
-  render() {
-    let classes = classNames("timeline-container", "panel", "panel-default");
+  let items = timeline.map((item) => {
     return (
-      <div className="col-md-6">
-        <div refCollection="timelineContainer" className={classes}>
-          <div className="panel-heading">
-            <h3 className="panel-title">Transcript</h3>
-          </div>
-          {this.renderList()}
+      <TimelineMessage
+      key={item.id}
+      id={item.id}
+      name={item.speakerName}
+      active={item.active}
+      clickEvent={clickEvent}
+      startTime={item.metStart}
+      text={item.text} />
+    );
+  });
+  return (
+    <div className="list-group">
+    {items}
+    </div>
+  );
+}
+
+export default function Timeline({timeline, clickEvent}) {
+  const classes = classNames(
+    "timeline-container",
+    "panel",
+    "panel-default"
+  );
+
+  return (
+    <div className="col-md-6">
+      <div testRefCollection="timelineContainer" className={classes}>
+        <div className="panel-heading">
+          <h3 className="panel-title">Transcript</h3>
         </div>
+        <TimelineList
+          timeline={timeline}
+          clickEvent={clickEvent} />
       </div>
-    );
-  }
+    </div>
+  );
 }
