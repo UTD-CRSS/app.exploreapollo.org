3c4881a9b9fcf2a25beec3a8846fcc24a669a42d
Austin Pray <austin@austinpray.com>
2016-02-07 14:43:12 -0600

    Refactors StoryList component to functional style

--- src/components/StoryList/index.js
+++ src/components/StoryList/index.js
@@ -2,50 +2,46 @@ import React, {Component} from "react";
 import {Link} from "react-router";
 import {keys} from "lodash";
 
-export class StoryListItem extends Component {
-  render() {
-    const {id, title, description} = this.props;
-    return (
-      <div className="panel panel-default">
-        <div className="panel-body">
-          <h2 testRef="storyTitle">{title}</h2>
-          <p className="lead">{description}</p>
-          <p>
-            <Link
-              className="btn btn-primary btn-lg"
-              testRef="storyLink"
-              to={`/stories/story/${id}`}>
-              Launch
-            </Link>
-          </p>
-        </div>
+export function StoryListItem({id, title, description}) {
+  return (
+    <div className="panel panel-default">
+      <div className="panel-body">
+        <h2 testRef="storyTitle">{title}</h2>
+        <p className="lead">{description}</p>
+        <p>
+          <Link
+            className="btn btn-primary btn-lg"
+            testRef="storyLink"
+            to={`/stories/story/${id}`}>
+            Launch
+          </Link>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default class StoryList extends Component {
-  renderList() {
-    const {stories} = this.props;
-    if (!stories || stories.length < 1) {
-      return (
-        <div testRef="errorMessage" className="alert alert-info">No Stories</div>
-      );
-    }
-    return keys(stories).map((index) => {
-      return (
-        <StoryListItem
-          key={stories[index].id}
-          {...stories[index]} />
-      );
-    });
-  }
-
-  render() {
+function renderStoryListItems(stories) {
+  if (!stories || stories.length < 1) {
     return (
-      <div testRefCollection="storyListContainer">
-        {this.renderList()}
+      <div testRef="errorMessage" className="alert alert-info">
+        No Stories
       </div>
     );
   }
+  return keys(stories).map((index) => {
+    return (
+      <StoryListItem
+        key={stories[index].id}
+        {...stories[index]} />
+    );
+  });
+}
+
+export default function StoryList({stories}) {
+  return (
+    <div testRefCollection="storyListContainer">
+      {renderStoryListItems(stories)}
+    </div>
+  );
 }
