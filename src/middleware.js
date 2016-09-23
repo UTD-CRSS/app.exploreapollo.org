7049664869146822272d8dd1eebe8d20559210ad
Cole Dean Shepherd <cole@coledeanshepherd.com>
2016-09-23 00:55:14 -0500

    Switched to double quoted strings.

--- src/middleware.js
+++ src/middleware.js
@@ -5,11 +5,11 @@ import { RECEIVE_AUDIO } from "./actions";
 // The constant isn't exported from redux-router so I'm having to redefine it.
 // It's a bit of a hack and makes the Google Analytics code dependent on a version of redux-router.
 // An alternative way to do this is subscribe to the react router, but then you can't handle events the same way.
-const ROUTER_DID_CHANGE = '@@reduxReactRouter/routerDidChange';
+const ROUTER_DID_CHANGE = "@@reduxReactRouter/routerDidChange";
 
-const gaMomentEventCategory = 'Moment';
-const gaPlayEventAction = 'play';
-const gaPauseEventAction = 'pause';
+const gaMomentEventCategory = "Moment";
+const gaPlayEventAction = "play";
+const gaPauseEventAction = "pause";
 
 export function googleAnalytics(store) {
   // Return a function handling actions.
@@ -17,19 +17,19 @@ export function googleAnalytics(store) {
     switch (action.type) {
       // Send a pageview when the route changes.
       case ROUTER_DID_CHANGE:
-        ga('set', 'page', action.payload.location.pathname + action.payload.location.search);
-        ga('send', 'pageview');
+        ga("set", "page", action.payload.location.pathname + action.payload.location.search);
+        ga("send", "pageview");
         break;
       
       // Send moment play/pause events.
       case RECEIVE_AUDIO:
         // This if statement is an optimization to avoid running code every time the app receives audio (often).
-        if(typeof action.playing !== 'undefined') {
+        if(typeof action.playing !== "undefined") {
           const state = store.getState();
           const gaEventAction = action.playing ? gaPlayEventAction : gaPauseEventAction;
           const timeInMilliseconds = Math.round(1000 * state.audio.time);
 
-          ga('send', 'event', gaMomentEventCategory, gaEventAction, state.audio.momentId, timeInMilliseconds);
+          ga("send", "event", gaMomentEventCategory, gaEventAction, state.audio.momentId, timeInMilliseconds);
         }
     }
 
