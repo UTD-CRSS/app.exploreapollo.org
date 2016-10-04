e9ce7cf6be761ea1c989510b37ab725ee7e988c1
Austin Pray <austin@austinpray.com>
2016-10-03 21:05:40 -0500

    Staging GA Code

--- config/staging.js
+++ config/staging.js
@@ -1,5 +1,5 @@
 // staging config
 module.exports = {
   apiEntry: "https://exploreapollo-api-staging.herokuapp.com",
-  GoogleAnalyticsCode: "UA-83921870-1"
+  GoogleAnalyticsCode: "UA-83921870-2"
 };
