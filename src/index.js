ed6af5a64eabfb907b901f7a790c3793e9ca7211
Kelsey <41966939+kehess99@users.noreply.github.com>
2020-09-28 16:28:47 -0500

    Fixed routing so that links between pages and urls work now

--- src/index.js
+++ src/index.js
@@ -1,58 +1,21 @@
 import "./scss/main.scss";
 import React from "react";
 import ReactDom from "react-dom";
-// import { render } from "react-dom";
 import { Provider } from "react-redux";
-//import { ReduxRouter } from "redux-router";
 //import configureStore from "./store/configureStore";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import  Routes  from "./routes";
 
-import {
-  Dashboard,
-  NoMatch,
-  Moments,
-  MomentViewer,
-  Stories,
-  StoryViewer,
-  App,
-  RandomMoment,
-  Settings,
-  PlaylistViewer,
-  Search,
-  DJ,
-  Apollo11Explorer,
-  Game
-} from "./containers";
 
 if (location.href.indexOf("#") != -1) {
   history.replaceState({} , "", `${location.hash.substring(1)}`);
 }
 
-//const store = configureStore();
 
 ReactDom.render(
- // <Provider store={store}>
     <BrowserRouter>
       <Switch>
-      {/* <Route name="app" path="/" component={App} />
-        <Route path="settings" component={Settings} />
-        <Route path="moments/moment/:momentId" component={MomentViewer} />
-        <Route path="moments" component={Moments} />
-        <Route path="stories/story/:storyId" component={StoryViewer} />
-        <Route path="stories/story/:storyId/moment" component={PlaylistViewer}>
-          <Route path=":momentId" component={MomentViewer} />
-        </Route>
-        <Route path="search" component={Search} />
-        <Route path="stories" component={Stories}/>
-        <Route path="apollo11" component={Apollo11Explorer}/>
-        <Route path="apollo11/day/:missionDay" component={Apollo11Explorer}/>
-        <Route path="moments/random" component={RandomMoment} />
-        <Route path="dj" component={DJ} />
-        <Route path="game" component={Game} />
-        <Route path="*" component={NoMatch}/> */}
         <Routes />
       </Switch>
     </BrowserRouter>,
-  //</Provider>
 document.getElementById("houston"));
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 
 
 import {
-  Dashboard,
   NoMatch,
   Moments,
   MomentViewer,
@@ -23,24 +22,23 @@ export default class Routes extends Component {
   render() {
     return (
     <Switch>
-      <Route name="app" path="/" component={App} ></Route>
-        <Route path="settings" component={Settings} />
-        <Route path="moments/moment/:momentId" component={MomentViewer} />
-        <Route path="moments" component={Moments} />
-        <Route path="stories/story/:storyId" component={StoryViewer} />
-        <Route path="stories/story/:storyId/moment" component={PlaylistViewer}>
-          <Route path=":momentId" component={MomentViewer} />
+      <Route name="app" exact path="/" component={App} ></Route>
+        <Route path="/settings" component={Settings} />
+        <Route path="/moments/moment/:momentId" component={MomentViewer} />
+        <Route path="/moments" component={Moments} />
+        <Route path="/stories/story/:storyId" component={StoryViewer} />
+        <Route path="/stories/story/:storyId/moment" component={PlaylistViewer}>
+          <Route path="/:momentId" component={MomentViewer} />
         </Route>
-        <Route path="search" component={Search} />
-        <Route path="stories" component={Stories}/>
-        <Route path="apollo11" component={Apollo11Explorer}/>
-        <Route path="apollo11/day/:missionDay" component={Apollo11Explorer}/>
-        <Route path="moments/random" component={RandomMoment} />
-        <Route path="dj" component={DJ} />
-        <Route path="game" component={Game} />
+        <Route path="/search" component={Search} />
+        <Route path="/stories" component={Stories}/>
+        <Route path="/apollo11" component={Apollo11Explorer}/>
+        <Route path="/apollo11/day/:missionDay" component={Apollo11Explorer}/>
+        <Route path="/moments/random" component={RandomMoment} />
+        <Route path="/dj" component={DJ} />
+        <Route path="/game" component={Game} />
         <Route path="*" component={NoMatch}/>
       </Switch>
-    //</Route>
   )
 }
 }
