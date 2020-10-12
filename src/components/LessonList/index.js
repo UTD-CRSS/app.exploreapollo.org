cf825d02732b77834002de01fe3cd242c7eb6250
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2020-10-12 17:02:04 -0500

    commit on 10/12/2020

--- src/components/LessonList/index.js
+++ src/components/LessonList/index.js
@@ -7,9 +7,9 @@ export function LessonList() {
 
         <div>
             Lesson plans are categorized as follows: 
-            K-2
-            3-5
-            6-8
+            K-2,
+            3-5,
+            6-8, and
             9-12
         </div>
 
