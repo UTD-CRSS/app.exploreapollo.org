1bf9b1aafe80169c107028dbcc46543dc72405ab
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2020-10-12 17:08:01 -0500

    change made: lesson plan category list

--- src/components/LessonList/index.js
+++ src/components/LessonList/index.js
@@ -6,11 +6,11 @@ export function LessonList() {
     return (
 
         <div>
-            Lesson plans are categorized as follows: 
-            K-2,
-            3-5,
-            6-8, and
-            9-12
+            Lesson plans are categorized as follows: {"\n"}
+            K-2 {"\n"}
+            3-5 {"\n"}
+            6-8 {"\n"}
+            9-12{"\n"}
         </div>
 
     );
