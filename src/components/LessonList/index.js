62cd168b92530cb5e0c1f3f2ab36c884797f3432
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2020-10-13 17:42:49 -0500

    commit on 10/13/2020

--- src/components/LessonList/index.js
+++ src/components/LessonList/index.js
@@ -8,8 +8,29 @@ export function LessonList() {
         <div>
             Lesson plans are categorized as follows: 
             K-2, 3-5, 6-8, and 9-12
-        </div>
 
+            K-2: 
+                Who is Neil Armstrong?
+                Tech 
+                Practical/Activities
+
+            3-5: 
+            History
+            Tech 
+            Practical/Activities
+
+            6-8: 
+            History
+            Tech 
+            Practical/Activities
+
+            9-12: 
+            History
+            Tech 
+            Practical/Activities 
+        
+        </div>
+           
     );
 
 }
