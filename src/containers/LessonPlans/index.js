2fbc7ab8a18e181d6f53890f4f01e19ff906eaea
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2020-10-15 20:44:57 -0500

    commit on 10/15/2020

--- src/containers/LessonPlans/index.js
+++ src/containers/LessonPlans/index.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { LessonList } from "../../components";
+import {Dashboard} from "../Dashboard";
 
-export class LessonPlans extends Component {
-
-    render() {
-
-
-        return (
-
-            <div>
-
-                <h1>Lessons</h1>
-                <LessonList />
+export class LessonPlans extends Component 
+{
+  render() 
+  {
+    return (
+      <nav className="navbar navbar-brand navbar-expand-xl navbar-inverse">
+        <div className="container-fluid">
+            <Link to="/" className="ExploreApollo">
+              Explore Apollo
+            </Link>
+        <div className="navitem">
+        <button className="navbtn">Lesson Plans</button>
+        <div class="dropdown-content">
+              <a href="#">K-2</a>
+              <a href="#">3-5</a>
+              <a href="#">6-8</a>
+              <a href="#">9-12</a>
             </div>
-
-        );
-
-    }
-
+        </div>
+    );
+  }
 }
