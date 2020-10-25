ee9e4ee65e7f72fbc46167dceadde337e7a12455
Kelsey <41966939+kehess99@users.noreply.github.com>
2020-10-25 18:55:26 -0500

    Changed indents for lint

--- src/components/InTheNewsComponent/index.js
+++ src/components/InTheNewsComponent/index.js
@@ -1,143 +1,143 @@
 import "./index.scss";
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export class InTheNewsComponent extends Component {
-    render() {
-        return(
-                <div className="newsLinks">
+        render() {
+                return (
+                        <div className="newsLinks">
 
-                <font></font>
-                <h1><b><u>Check out the articles we've been in!</u></b><br></br></h1>
+                                <font></font>
+                                <h1><b><u>Check out the articles we've been in!</u></b><br></br></h1>
 
-                <h4><b>1. UTDallas News<br></br>Published: December 5, 2017</b><br></br></h4>
-                <a href="https://www.utdallas.edu/news/research/researchers-launch-moon-mission-audio-site/" target="_blank">
-                Researchers Launch Moon Mission Audio Site
-                </a>
+                                <h4><b>1. UTDallas News<br></br>Published: December 5, 2017</b><br></br></h4>
+                                <a href="https://www.utdallas.edu/news/research/researchers-launch-moon-mission-audio-site/" target="_blank">
+                                        Researchers Launch Moon Mission Audio Site
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>2. Dallas Innovates<br></br>Published: December 7, 2017</b><br></br></h4>
-        <a href="https://dallasinnovates.com/utd-team-reconstructs-apollo-mission-recordings/" target="_blank">
-UTD Team Reconstructs Apollo Mission Recordings
-</a>
+                                <h4><b>2. Dallas Innovates<br></br>Published: December 7, 2017</b><br></br></h4>
+                                <a href="https://dallasinnovates.com/utd-team-reconstructs-apollo-mission-recordings/" target="_blank">
+                                        UTD Team Reconstructs Apollo Mission Recordings
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>3. National Science Foundation<br></br>Published: December 7, 2017</b><br></br></h4>
-        <a href="https://www.nsf.gov/news/news_summ.jsp?cntn_id=244009" target="_blank">
-    UT Dallas researchers develop techniques to analyze thousands of hours of Apollo mission audio 
-</a>
+                                <h4><b>3. National Science Foundation<br></br>Published: December 7, 2017</b><br></br></h4>
+                                <a href="https://www.nsf.gov/news/news_summ.jsp?cntn_id=244009" target="_blank">
+                                        UT Dallas researchers develop techniques to analyze thousands of hours of Apollo mission audio
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>4. Kera News<br></br>Published: December 20, 2017</b><br></br></h4>
-        <a href="https://www.keranews.org/health-science-tech/2017-12-20/listen-to-unheard-audio-from-nasas-apollo-missions-dusted-off-by-ut-dallas-researchers" target="_blank">
-    Listen To Unheard Audio From NASA’s Apollo Missions, Dusted Off By UT Dallas Researchers 
-</a>
+                                <h4><b>4. Kera News<br></br>Published: December 20, 2017</b><br></br></h4>
+                                <a href="https://www.keranews.org/health-science-tech/2017-12-20/listen-to-unheard-audio-from-nasas-apollo-missions-dusted-off-by-ut-dallas-researchers" target="_blank">
+                                        Listen To Unheard Audio From NASA’s Apollo Missions, Dusted Off By UT Dallas Researchers
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>5. Dmagazine<br></br>Published: December 21, 2017</b><br></br></h4>
-        <a href="https://www.dmagazine.com/frontburner/2017/12/ut-dallas-nasa-apollo-missions-audio/" target="_blank">
-    UT Dallas Researchers Help You Listen to the Apollo Missions
-</a>
+                                <h4><b>5. Dmagazine<br></br>Published: December 21, 2017</b><br></br></h4>
+                                <a href="https://www.dmagazine.com/frontburner/2017/12/ut-dallas-nasa-apollo-missions-audio/" target="_blank">
+                                        UT Dallas Researchers Help You Listen to the Apollo Missions
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>6. Collect Space news<br></br>Published: January 5, 2018</b><br></br></h4>
-        <a href="http://www.collectspace.com/news/news-010518a-explore-apollo-audio-research.html" target="_blank">
-    Researchers analyze NASA moon mission audio to let public 'Explore Apollo'
-</a>
-<br></br>
+                                <h4><b>6. Collect Space news<br></br>Published: January 5, 2018</b><br></br></h4>
+                                <a href="http://www.collectspace.com/news/news-010518a-explore-apollo-audio-research.html" target="_blank">
+                                        Researchers analyze NASA moon mission audio to let public 'Explore Apollo'
+                                </a>
+                                <br></br>
 
-<h4><b>7. Houston Public Media<br></br>Published: January 30, 2018</b><br></br></h4>
-        <a href="https://www.houstonpublicmedia.org/articles/news/2018/01/30/264541/what-researchers-found-on-hours-and-hours-of-audio-tapes-from-nasas-apollo-missions/" target="_blank">
-    What Researchers Found On Hours And Hours Of Audio Tapes From NASA’s Apollo Missions
-</a>
+                                <h4><b>7. Houston Public Media<br></br>Published: January 30, 2018</b><br></br></h4>
+                                <a href="https://www.houstonpublicmedia.org/articles/news/2018/01/30/264541/what-researchers-found-on-hours-and-hours-of-audio-tapes-from-nasas-apollo-missions/" target="_blank">
+                                        What Researchers Found On Hours And Hours Of Audio Tapes From NASA’s Apollo Missions
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>8. Texas Country Reporter<br></br>Published: May 31, 2019</b><br></br></h4>
-        <a href="https://www.youtube.com/watch?v=CTJtRNMac0E" target="_blank">
-        “Explore Apollo” Audio Archive
-</a>
+                                <h4><b>8. Texas Country Reporter<br></br>Published: May 31, 2019</b><br></br></h4>
+                                <a href="https://www.youtube.com/watch?v=CTJtRNMac0E" target="_blank">
+                                        “Explore Apollo” Audio Archive
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>9. NASA<br></br>Published: July 24, 2018</b><br></br></h4>
-        <a href=": https://www.nasa.gov/feature/nasa-university-of-texas-at-dallas-reveal-apollo-11-behind-the-scenes-audio" target="_blank">
-    University of Texas at Dallas Reveal Apollo 11 Behind-the-Scenes Audio
-</a>
+                                <h4><b>9. NASA<br></br>Published: July 24, 2018</b><br></br></h4>
+                                <a href=": https://www.nasa.gov/feature/nasa-university-of-texas-at-dallas-reveal-apollo-11-behind-the-scenes-audio" target="_blank">
+                                        University of Texas at Dallas Reveal Apollo 11 Behind-the-Scenes Audio
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>10. NASA<br></br>Published: August 25, 2018</b><br></br></h4>
-        <a href="https://www.nbcnews.com/mach/science/nasa-releases-19-000-historic-audio-11-mission-ncna903721" target="_blank">
-    NASA releases 19,000 hours of audio from historic Apollo 11 mission 
-</a>
+                                <h4><b>10. NASA<br></br>Published: August 25, 2018</b><br></br></h4>
+                                <a href="https://www.nbcnews.com/mach/science/nasa-releases-19-000-historic-audio-11-mission-ncna903721" target="_blank">
+                                        NASA releases 19,000 hours of audio from historic Apollo 11 mission
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>11. Discover Magazine<br></br>Published: October 29, 2018</b><br></br></h4>
-        <a href="https://www.discovermagazine.com/the-sciences/hear-the-backstage-story-of-the-apollo-program-with-newly-released-audio" target="_blank">
-        Hear the Backstage Story of the Apollo Program With Newly Released Audio 
-</a>
+                                <h4><b>11. Discover Magazine<br></br>Published: October 29, 2018</b><br></br></h4>
+                                <a href="https://www.discovermagazine.com/the-sciences/hear-the-backstage-story-of-the-apollo-program-with-newly-released-audio" target="_blank">
+                                        Hear the Backstage Story of the Apollo Program With Newly Released Audio
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>12. Vanity Fair<br></br>Published: December 9, 2018</b><br></br></h4>
-        <a href="https://www.vanityfair.com/hollywood/2018/12/apollo-11-50th-year-anniversary" target="_blank">
-    The Found Footage That Provides a Whole New Look at the Apollo 11 Moon Landing
-</a>
+                                <h4><b>12. Vanity Fair<br></br>Published: December 9, 2018</b><br></br></h4>
+                                <a href="https://www.vanityfair.com/hollywood/2018/12/apollo-11-50th-year-anniversary" target="_blank">
+                                        The Found Footage That Provides a Whole New Look at the Apollo 11 Moon Landing
+                                </a>
 
-<br></br>
+                                <br></br>
 
-<h4><b>13. NASA Heroes behind the Heroes<br></br>Published: December 5, 2017</b><br></br>
-    This is a 4 part Podcast.<br></br></h4>
-    <a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep88_heroesbehindheroes_pt1.mp3" target="_blank">
-        Podcast: Part 1
-</a>
-<br></br>
-    <a href="https://www.nasa.gov/johnson/HWHAP/heroes-behind-the-heroes" target="_blank">
-            News Article: Part 1
-</a>
-<br></br>
-<a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep89_heroesbehindheroes_pt2.mp3" target="_blank">
-        Podcast: Part 2
-</a>
-<br></br>
-<a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-2" target="_blank">
-        News Article: Part 2
-</a>
-<br></br>
-<a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep90_heroesbehindheroes_pt3.mp3" target="_blank">
-        Podcast: Part 3
-</a>
-<br></br>
-<a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-3" target="_blank">
-        News Article: Part 3
-</a>
-<br></br>
-<a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep91_heroesbehindheroes_pt4.mp3" target="_blank">
-        Podcast: Part 4
-</a>
-<br></br>
-<a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-4" target="_blank">
-        News Article: Part 4
-</a>
-<br></br>    
+                                <h4><b>13. NASA Heroes behind the Heroes<br></br>Published: December 5, 2017</b><br></br>
+                                This is a 4 part Podcast.<br></br></h4>
+                                <a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep88_heroesbehindheroes_pt1.mp3" target="_blank">
+                                        Podcast: Part 1
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/johnson/HWHAP/heroes-behind-the-heroes" target="_blank">
+                                        News Article: Part 1
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep89_heroesbehindheroes_pt2.mp3" target="_blank">
+                                        Podcast: Part 2
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-2" target="_blank">
+                                        News Article: Part 2
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep90_heroesbehindheroes_pt3.mp3" target="_blank">
+                                        Podcast: Part 3
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-3" target="_blank">
+                                        News Article: Part 3
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/sites/default/files/atoms/audio/ep91_heroesbehindheroes_pt4.mp3" target="_blank">
+                                        Podcast: Part 4
+                                </a>
+                                <br></br>
+                                <a href="https://www.nasa.gov/johnson/HWHAP/the-heroes-behind-the-heroes-part-4" target="_blank">
+                                        News Article: Part 4
+                                </a>
+                                <br></br>
 
-<h4><b>14. FOX News<br></br>Published: July 19, 2019</b><br></br></h4>
-        <a href="https://video.foxnews.com/v/6061639816001#sp=show-clips" target="_blank">
-    Engineers restore audio recordings from Apollo 11 mission
-</a>
-<br></br>
+                                <h4><b>14. FOX News<br></br>Published: July 19, 2019</b><br></br></h4>
+                                <a href="https://video.foxnews.com/v/6061639816001#sp=show-clips" target="_blank">
+                                        Engineers restore audio recordings from Apollo 11 mission
+                                </a>
+                                <br></br>
 
-<h4><b>15. Dallas Morning News<br></br>Published: December 5, 2017</b><br></br></h4>
-        <a href="https://www.dallasnews.com/news/education/2019/07/20/ut-dallas-celebrates-personal-ties-with-nasa-50-years-after-one-small-step-for-man/" target="_blank">
-    UT-Dallas celebrates personal ties with NASA 50 years after ‘one small step for man’
-</a>
-       </div>
-    );
-    }
+                                <h4><b>15. Dallas Morning News<br></br>Published: December 5, 2017</b><br></br></h4>
+                                <a href="https://www.dallasnews.com/news/education/2019/07/20/ut-dallas-celebrates-personal-ties-with-nasa-50-years-after-one-small-step-for-man/" target="_blank">
+                                        UT-Dallas celebrates personal ties with NASA 50 years after ‘one small step for man’
+                                </a>
+                        </div>
+                );
+        }
 }
