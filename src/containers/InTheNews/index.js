2ffe8c220643c6fb11a3cb377bbdac8dd00e8c67
unknown <naurin.nasim@gmail.com>
2020-10-25 18:40:44 -0500

    In the news image

--- src/containers/InTheNews/index.js
+++ src/containers/InTheNews/index.js
@@ -1,18 +1,27 @@
-import React, {Component} from "react";
-import {Link} from "react-router";
+import React, { Component } from "react";
+import { Link } from "react-router";
 import { connect } from "react-redux";
 import { Dashboard } from "../Dashboard";
-import {InTheNewsComponent} from "../../components/InTheNewsComponent";
+import inthenews from '../../../static/inthenews.png';
+import { InTheNewsComponent } from "../../components/InTheNewsComponent";
 
 export default class InTheNews extends Component {
-    render() {
-      return (
-          <div className="app-container">
-             <h1>We've been in the news! Check out the buzz (aldrin, haha). </h1> 
-             <InTheNewsComponent />
-             </div>
+  render() {
+    return (
+      <div className="app-container">
+
+        <h1>We've been in the news! Check out the buzz! </h1>
+        <div className="row">
+          <div className="col-md-7" >
+            <img src={inthenews} />
+          </div>
+          <div className="col-md-5">
+            <InTheNewsComponent />
+          </div>
+        </div>
+      </div>
 
 
-      );
-        }
+    );
+  }
 }
