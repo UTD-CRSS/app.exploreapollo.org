122d34ea99084663c50d954df8057a111d56d451
Kelsey <41966939+kehess99@users.noreply.github.com>
2020-10-25 13:25:50 -0500

    Got InTheNews to show up

--- src/containers/InTheNews/index.js
+++ src/containers/InTheNews/index.js
@@ -2,14 +2,15 @@ import React, {Component} from "react";
 import {Link} from "react-router";
 import { connect } from "react-redux";
 import { Dashboard } from "../Dashboard";
+import {InTheNewsComponent} from "../../components/InTheNewsComponent";
 
-
-export class InTheNews extends Component {
+export default class InTheNews extends Component {
     render() {
       return (
-          <div className ="app-container">
+          <div className="app-container">
              <h1>Welcome to In The News </h1> 
-          </div>
+             <InTheNewsComponent />
+             </div>
       );
         }
 }
