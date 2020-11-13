eb5a86360eb4ae059ce5845eb01d2ac33b4f8743
Kirtana Kalavagunta <kirtana.ks@gmail.com>
2020-11-13 13:33:00 -0600

    commit on 11/13

---
+++ src/containers/Apollo6Explorer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { AppFooter, AppHeader } from "../App";
+
+export class Apollo6Explorer extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <AppHeader />
+        <h1>Apollo 6 </h1>
+        <AppFooter />
+      </div>
+    );
+  }
+}
