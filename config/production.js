463cdaadcf41e5c0910f1e05b86f6f0a8b558d7f
SoursopWanderer <184578764+SoursopWanderer@users.noreply.github.com>
2025-02-26 19:47:02 -0600

    Update API entry

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 // production config
 module.exports = {
-  apiEntry: "https://exploreapollo-api.herokuapp.com",
+  apiEntry: "https://exploreapollo-api-acaa71162e15.herokuapp.com",
   GoogleAnalyticsCode: "UA-83921870-1"
 };
